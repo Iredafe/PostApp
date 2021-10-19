888fc625dfffa5dd829a7bea0f31e57051c4001e
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-18 23:19:48 -0600

    BUILDING ACTION CREATOR III

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,9 +3,12 @@ import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
 class PostList extends React.Component{
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
     render(){
         return <div>Post List</div>
     }
  }
 
-export default connect()(PostList);
+export default connect(null, {fetchPosts})(PostList);
